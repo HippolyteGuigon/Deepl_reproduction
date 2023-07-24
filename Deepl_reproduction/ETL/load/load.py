2d1ce447491973a592020ae460009eceb3df4f09
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-24 19:53:47 +0200

    pushing_latest_advance

--- Deepl_reproduction/ETL/load/load.py
+++ Deepl_reproduction/ETL/load/load.py
@@ -1,3 +1,25 @@
+import logging
+import os
 from google.cloud import bigquery
+from Deepl_reproduction.ETL.extract.wikipedia_source import get_wikipedia_article
+from Deepl_reproduction.logs.logs import main
 
-client = bigquery.Client()
+project="deepl-reproduction"
+
+client = bigquery.Client(project=project)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+
+main()
+
+def load_data(data,dataset="raw_data",table_name="raw_wikipedia", client=client)->None:
+    table_ref=client.dataset(dataset).table(table_name)
+    client.insert_rows_json(table_ref,data)
+    logging.info(f"Data were successfully pushed in dataset {dataset} in table {table_name}")
+
+if __name__=="__main__":
+    page, content=get_wikipedia_article()
+    data=[{"page_name":page, "content":content}]
+    load_data(data)
--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 activate_environment:
 	mamba init
-	mamba activate deepl_reproduction_environment
+	mamba activate deepl_reproduction_env
 
 setup:
 	python3 setup.py install
+
+connect_gcp:
+	gcloud auth login
+
+load_google_creditentials:
+	export GOOGLE_APPLICATION_CREDENTIALS=deepl_api_key.json
