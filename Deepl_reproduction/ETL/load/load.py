43940727aee468c4a612d837be1ce1794d350b30
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-25 11:05:12 +0200

    pushing_load

--- Deepl_reproduction/ETL/load/load.py
+++ Deepl_reproduction/ETL/load/load.py
@@ -6,7 +6,7 @@ from Deepl_reproduction.logs.logs import main
 
 project="deepl-reproduction"
 
-client = bigquery.Client(project=project)
+client = bigquery.Client()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,10 +14,13 @@ formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
 main()
 
-def load_data(data,dataset="raw_data",table_name="raw_wikipedia", client=client)->None:
-    table_ref=client.dataset(dataset).table(table_name)
-    client.insert_rows_json(table_ref,data)
-    logging.info(f"Data were successfully pushed in dataset {dataset} in table {table_name}")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "deepl_api_key.json"
+
+def load_data(data, project_id="deepl-reproduction", dataset_id="raw_data", table_name="raw_wikipedia", client=bigquery.Client()) -> None:
+    table_ref=client.dataset(dataset_id).table(table_name)
+    client.insert_rows_json(table_ref, data)
+    logging.info(f"Data were successfully pushed in dataset {dataset_id} in table {table_name}")
+
 
 if __name__=="__main__":
     page, content=get_wikipedia_article()
