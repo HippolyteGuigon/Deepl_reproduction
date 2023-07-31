d0f99bd2197a892a58744e234536df1a543decf0
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-07-31 19:04:36 +0200

    pushing_corrected

--- Deepl_reproduction/ETL/transform/transform_wikipedia.py
+++ Deepl_reproduction/ETL/transform/transform_wikipedia.py
@@ -25,6 +25,6 @@ def treat_article(article: str)->List[str]:
 
     return modified_article
 
-def translate_content(df: pd.DataFrame, input_language: str="fr", output_language: str="en")->pd.DataFrame:
-    df["content_translated"]=df["content"].apply(lambda texte: translate_text(text=texte, source_lang=input_language, target_lang=output_language))
+def translate_content(df: pd.DataFrame, output_language: str="en")->pd.DataFrame:
+    df["content_translated"]=df["content"].apply(lambda texte: translate_text(text=texte, target_lang=output_language))
     return df
