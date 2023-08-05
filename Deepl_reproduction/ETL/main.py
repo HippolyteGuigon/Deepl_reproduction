d76ffe3f14367ffc6f3b7f421d74cc4a75a5fb04
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-05 16:10:51 +0200

    pushing_proper_path

--- Deepl_reproduction/ETL/main.py
+++ Deepl_reproduction/ETL/main.py
@@ -19,7 +19,11 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 logging.warning(f"The current workspace is: {os.getcwd()}, and the files inside are {os.listdir()}")
 
-sys.path.insert(0,os.path.join(os.getcwd(),"Deepl_reproduction"))
+if "workspace" in os.getcwd():
+    current_path = os.path.abspath(os.getcwd())
+    parent_path = os.path.dirname(os.path.dirname(current_path))
+    os.chdir(parent_path)
+    
 from Deepl_reproduction.configs.confs import load_conf, clean_params
 from Deepl_reproduction.ETL.extract.wikipedia_source import get_wikipedia_article
 from Deepl_reproduction.ETL.transform.transform_wikipedia import treat_article, translate_content
