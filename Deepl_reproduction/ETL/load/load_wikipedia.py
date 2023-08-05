975d9d6394cf6722b66848316af3c73bd6293823
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-05 16:05:09 +0200

    pushing_main

--- Deepl_reproduction/ETL/load/load_wikipedia.py
+++ Deepl_reproduction/ETL/load/load_wikipedia.py
@@ -16,6 +16,9 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
+if "workspace" not in os.getcwd():
+    main()
+    
 def indexing(data: pd.DataFrame)->pd.DataFrame:
     """
     The goal of this funcction is, 
