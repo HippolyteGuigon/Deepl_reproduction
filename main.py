3f22dececaf1c1f0a158ad25ad3e9a733b9fb901
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-07 23:30:43 +0200

    pushing_main_and_requirements

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import functions_framework
 from flask import Flask, request
 from Deepl_reproduction.ETL.wikipedia_cloud_function import wikipedia_etl_pipeline
+from Deepl_reproduction.ETL.eventregistry_cloud_function import eventregistry_etl_pipeline
+
 
 @functions_framework.http
 def launch_wikipedia_pipeline(request)->None:
@@ -16,5 +18,22 @@ def launch_wikipedia_pipeline(request)->None:
         -None
     """
     wikipedia_etl_pipeline(request)
+
+    return "Success"
+
+@functions_framework.http
+def launch_eventregistry_pipeline(request)->None:
+    """
+    The goal of this function 
+    is to launch the eventregistry
+    pipeline ETL process cloud 
+    function 
+    
+    Arguments:
+        -requests
+    Returns:
+        -None
+    """
+    eventregistry_etl_pipeline(request)
 
     return "Success"
