014de85a5af2f80a6444caf09070f61bb56c8904
Hippolyte <Hippolyte.guigon@polytechnique.edu>
2023-08-17 21:42:19 +0200

    pushing_multiheaded

--- Deepl_reproduction/model/model.py
+++ Deepl_reproduction/model/model.py
@@ -8,14 +8,14 @@ import numpy as np
 import seaborn as sns
 import torchtext
 import matplotlib.pyplot as plt
-warnings.simplefilter("ignore")
-print(torch.__version__)
-
 import sys 
+
+warnings.filterwarnings("ignore")
+
 sys.path.insert(0,"Deepl_reproduction/pipeline")
+
 from Deepl_reproduction.logs.logs import main
 from data_loading import load_all_data, load_data_to_front_database, load_data
-from sklearn.model_selection import  train_test_split
 
 class Embedding(nn.Module):
     def __init__(self, vocab_size: int, embed_dim: int):
@@ -121,3 +121,40 @@ class MuliHeadAttention(nn.Module):
         self.key_matrix=nn.Linear(self.single_head_dim,self.single_head_dim,bias=False)
         self.value_matrix=nn.Linear(self.single_head_dim,self.single_head_dim,bias=False)
         self.out=nn.Linear(self.n_heads*self.single_head_dim,self.embed_dim)
+
+    def forward(self, key, query, value, mask=None):
+
+        batch_size=key.size(0)
+        seq_length=key.size(1)
+
+        seq_length_query=query.size(1)
+
+        key=key.view(batch_size,seq_length,self.n_heads,self.single_head_dim)
+        query=query.view(batch_size,seq_length,self.n_heads,self.single_head_dim)
+        value=value.view(batch_size,seq_length,self.n_heads,self.single_head_dim)
+
+        k=self.key_matrix(key)
+        q=self.query_matrix(query)
+        v=self.value_matrix(value)
+
+        q=q.transpose(1, 2)
+        k=k.transpose(1, 2)
+        v=v.transpose(1,2)
+
+        k_adjusted=k.transpose(-1,-2)
+        product=torch.matmul(q,k_adjusted)
+
+        if mask is not None:
+            product=product.masked_fill(mask==0,float(1e-20))
+        
+        product=product/math.sqrt(self.single_head_dim)
+
+        scores=F.softmax(product,dim=-1)
+
+        scores=torch.matmul(scores,v)
+
+        concat=scores.transpose(1, 2).contiguous().view(batch_size,seq_length_query, self.single_head_dim*self.n_heads)
+
+        output=self.out(concat)
+
+        return output
