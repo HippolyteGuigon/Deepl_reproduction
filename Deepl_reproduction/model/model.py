250208d17d0dea88d6b8e72f480cc00f5272494c
Hippolyte <Hippolyte.guigon@polytechnique.edu>
2023-08-18 09:41:22 +0200

    pushing_full_transformer

--- Deepl_reproduction/model/model.py
+++ Deepl_reproduction/model/model.py
@@ -166,3 +166,127 @@ class TransformerBlock(nn.Module):
         self.attention=MuliHeadAttention(embed_dim,n_heads)
 
         self.norm1=nn.LayerNorm(embed_dim)
+        self.norm2=nn.LayerNorm(embed_dim)
+
+        self.feed_forward=nn.Sequential(nn.Linear(embed_dim,expansion_factor*embed_dim),
+                                        nn.ReLU(),
+                                        nn.Linear(expansion_factor*embed_dim,embed_dim)
+                                        )
+        
+        self.dropout1=nn.Dropout(0.2)
+        self.dropout2=nn.Dropout(0.2)
+
+
+    def forward(self,key, query, value):
+        attention_out=self.attention(key,query,value)
+        attention_residual_out=attention_out+value 
+        norm1_out=self.dropout1(self.norm1(attention_residual_out))
+
+        feed_fwd_out=self.feed_forward(norm1_out)
+
+        feed_fwd_residual_out=feed_fwd_out+norm1_out
+        norm2_out=self.dropout2(self.norm2(feed_fwd_residual_out))
+
+        return norm2_out
+    
+class TransformerEncoder(nn.Module):
+
+    def __init__(self,seq_len, vocab_size, embed_dim, num_layers=2, expansion_factor=4, n_heads=8):
+        super(TransformerEncoder,self).__init__()
+
+        self.embedding_layer=Embedding(vocab_size,embed_dim)
+        self.positional_encoder=PositionalEmbedding(seq_len,embed_dim)
+
+        self.layers=nn.ModuleList([TransformerBlock(embed_dim,expansion_factor,n_heads) for i in range(num_layers)])
+
+    def forward(self, x):
+        embed_out=self.embedding_layer(x)
+        out=self.positional_encoder(embed_out)
+        
+        for layer in self.layers:
+            out=layer(out,out,out)
+
+        return out
+    
+class DecoderBlock(nn.Module):
+    def __init__(self, embed_dim, expansion_factor=4,n_heads=8):
+        super(DecoderBlock,self).__init__()
+
+        self.attention=MuliHeadAttention(embed_dim,n_heads=8)
+        self.norm=nn.LayerNorm(embed_dim)
+        self.dropout=nn.Dropout(0.2)
+        self.transformer_block=TransformerBlock(embed_dim,expansion_factor, n_heads)
+
+    def forward(self, key, query, x, mask):
+
+        attention=self.attention(x, x, x, mask=mask)
+        value=self.dropout(self.norm(attention+x))
+
+        out=self.transformer_block(key, query, value)
+
+        return out
+    
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, target_vocab_size, embed_dim, seq_len, num_layers=2, expansion_factor=4, n_heads=8):
+        super(TransformerDecoder,self).__init__()
+
+        self.word_embedding=nn.Embedding(target_vocab_size,embed_dim)
+        self.position_embedding=PositionalEmbedding(seq_len, embed_dim)
+
+        self.layers=nn.ModuleList([DecoderBlock(embed_dim,expansion_factor=4,n_heads=8) for _ in range(num_layers)])
+
+        self.fc_out=nn.Linear(embed_dim,target_vocab_size)
+        self.dropout=nn.Dropout(0.2)
+
+    def forward(self, x, enc_out, mask):
+        x=self.word_embedding(x)
+        x=self.position_embedding(x)
+        x=self.dropout(x)
+
+        for layer in self.layers:
+            x=layer(enc_out,x, enc_out, mask)
+
+        out=F.softmax(self.fc_out(x))
+
+        return out 
+    
+
+class Transformer(nn.Module):
+    def __init__(self, embed_dim, src_vocab_size, target_vocab_size, seq_length, num_layers=2, expansion_factor=4,n_heads=8):
+        super(Transformer,self).__init__()
+
+        self.target_vocab_size=target_vocab_size
+        self.encoder=TransformerEncoder(seq_length, src_vocab_size, embed_dim, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads)
+        self.decoder=TransformerDecoder(target_vocab_size, embed_dim, seq_length, num_layers=num_layers,expansion_factor=expansion_factor, n_heads=n_heads)
+
+    def make_trg_mask(self, trg):
+        batch_size, trg_len = trg.shape
+
+        trg_mask=torch.trill(torch.ones((trg_len,trg_len))).expand(batch_size, 1, trg_len, trg_len)
+
+        return trg_mask
+    
+    def decode(self, src, trg):
+        trg_mask=self.make_trg_mask(trg)
+        enc_out=self.encoder(src)
+        out_labels=[]
+        batch_size, seq_len = src.shape[0], src.shape[1]
+
+        out=trg
+
+        for i in range(seq_len):
+            out=self.decoder(out, enc_out, trg_mask)
+            out=out[:,-1,:]
+            out=out.argmax(-1)
+            out_labels.append(out.item())
+            out=torch.unsqueeze(out,axis=0)
+
+        return out_labels
+    
+    def forward(self, src, trg):
+        trg_mask=self.make_trg_mask(trg)
+        enc_out=self.encoder(src)
+        outputs=self.decoder(trg, enc_out, trg_mask)
+
+        return outputs
