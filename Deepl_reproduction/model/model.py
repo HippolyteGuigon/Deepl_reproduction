b4867a8525a3110d7ce3065ca8671e9dd5b343f7
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-21 07:50:07 +0200

    pushing_lower_dim

--- Deepl_reproduction/model/model.py
+++ Deepl_reproduction/model/model.py
@@ -356,5 +356,5 @@ def fit_transformer(model, max_seq_length, batch_size=32, num_epochs=10, learnin
     logging.warning("Training finished.")
 
 if __name__=="__main__": 
-    model = Transformer(embed_dim=128, src_vocab_size=30000, target_vocab_size=30000, seq_length=128, num_layers=3, expansion_factor=2, n_heads=8)
-    fit_transformer(model, max_seq_length=128, batch_size=100, num_epochs=10, learning_rate=1e-3, device='cpu')
+    model = Transformer(embed_dim=64, src_vocab_size=10000, target_vocab_size=10000, seq_length=64, num_layers=3, expansion_factor=2, n_heads=8)
+    fit_transformer(model, max_seq_length=64, batch_size=100, num_epochs=10, learning_rate=1e-1, device='cpu')
