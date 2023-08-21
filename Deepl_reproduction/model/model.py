fa4005125995623e2ea5e8845fa44d9640c9bf78
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-21 15:29:09 +0200

    pushing_shorter_sentence

--- Deepl_reproduction/model/model.py
+++ Deepl_reproduction/model/model.py
@@ -357,4 +357,4 @@ def fit_transformer(model, max_seq_length, batch_size=32, num_epochs=10, learnin
 
 if __name__=="__main__": 
     model = Transformer(embed_dim=16, src_vocab_size=30000, target_vocab_size=20000, seq_length=64, num_layers=3, expansion_factor=2, n_heads=8)
-    fit_transformer(model, max_seq_length=32, batch_size=32, num_epochs=30, learning_rate=1e-3, device='cpu')
+    fit_transformer(model, max_seq_length=15, batch_size=32, num_epochs=30, learning_rate=1e-3, device='cpu')
