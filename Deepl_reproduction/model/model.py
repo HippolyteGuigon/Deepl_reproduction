63be241821cd33dd0b02fb42a399d3d08481a4ca
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-21 15:34:52 +0200

    pushin_fewer_vocab_size

--- Deepl_reproduction/model/model.py
+++ Deepl_reproduction/model/model.py
@@ -309,7 +309,7 @@ def fit_transformer(model, max_seq_length, batch_size=32, num_epochs=10, learnin
     src_sentences=df_front_database["french"].tolist()
     trg_sentences=df_front_database["english"].tolist()
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',hidden_size=16)  # Utilisez le tokenizer BERT
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',vocab_size = 10000)  # Utilisez le tokenizer BERT
     
     # Tokenize, encode, and pad source sentences
     src_tokens = [tokenizer.encode(text, add_special_tokens=True, max_length=max_seq_length, pad_to_max_length=True,truncation=True) for text in src_sentences]
@@ -356,5 +356,5 @@ def fit_transformer(model, max_seq_length, batch_size=32, num_epochs=10, learnin
     logging.warning("Training finished.")
 
 if __name__=="__main__": 
-    model = Transformer(embed_dim=16, src_vocab_size=5000, target_vocab_size=5000, seq_length=64, num_layers=3, expansion_factor=2, n_heads=8)
+    model = Transformer(embed_dim=16, src_vocab_size=10000, target_vocab_size=10000, seq_length=64, num_layers=3, expansion_factor=2, n_heads=8)
     fit_transformer(model, max_seq_length=15, batch_size=32, num_epochs=30, learning_rate=1e-3, device='cpu')
