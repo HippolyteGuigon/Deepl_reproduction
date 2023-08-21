9cc6671d21e778de369319847ee989099fe1f2b0
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-08-21 16:50:43 +0200

    pushing_proper_vocab_size

--- Deepl_reproduction/model/model.py
+++ Deepl_reproduction/model/model.py
@@ -354,5 +354,5 @@ def fit_transformer(model, max_seq_length, batch_size=32, num_epochs=10, learnin
     logging.warning("Training finished.")
 
 if __name__=="__main__": 
-    model = Transformer(embed_dim=16, src_vocab_size=30000, target_vocab_size=30000, seq_length=64, num_layers=3, expansion_factor=2, n_heads=8)
+    model = Transformer(embed_dim=16, src_vocab_size=50000, target_vocab_size=50000, seq_length=64, num_layers=3, expansion_factor=2, n_heads=8)
     fit_transformer(model, max_seq_length=15, batch_size=32, num_epochs=30, learning_rate=1e-3, device='cpu')
