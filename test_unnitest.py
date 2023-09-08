9b1d332ae3fa930431a7af8f8a6f1c763796e834
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-09-08 21:05:52 +0200

    pushing_corrected_test

--- test_unnitest.py
+++ test_unnitest.py
@@ -5,10 +5,18 @@ import random
 import sys
 from google.cloud import storage
 from Deepl_reproduction.ETL.transform.traduction import translate_text
-from Deepl_reproduction.pipeline.data_loading import load_all_data, load_data_to_front_database, load_data
+from Deepl_reproduction.pipeline.data_loading import (
+    load_data_to_front_database,
+    load_data,
+)
 from Deepl_reproduction.model.model_loading import load_model
 
-client = storage.Client.from_service_account_json('deepl_api_key.json', project='deepl-reprodution')
+client = storage.Client.from_service_account_json(
+    "deepl_api_key.json", project="deepl-reprodution"
+)
+
+sys.path.insert(0, "./Deepl_reproduction")
+
 
 class Test(unittest.TestCase):
     """
@@ -16,30 +24,39 @@ class Test(unittest.TestCase):
     and check everything commited makes sense
     """
 
-    def test_traduction_function(self)->None:
+    def test_traduction_function(self) -> None:
         """
         The goal of this function is to test
-        the traduction function works and 
-        is able to deliver accuracte results 
-        
+        the traduction function works and
+        is able to deliver accuracte results
+
         Arguments:
             -None
         Returns:
             -None
         """
 
-        languages=["FR", "JA", "EN-GB", "ES"]
-        sentence_to_translate={"Hey, how are you ?":"EN", 
-                               "Bonjour, comment ça va ?":"FR",
-                               "Hola, ¿cómo estás?": "ES", 
-                               "こんにちは、お元気ですか?":"JA"}
-        
-        sentence_translated=[translate_text(sentence,target_lang=target_lang) for sentence in sentence_to_translate.keys() for target_lang in languages if sentence_to_translate[sentence]!=target_lang]
-        is_all_sentence=all([isinstance(sentence,str) for sentence in sentence_translated])
+        languages = ["FR", "JA", "EN-GB", "ES"]
+        sentence_to_translate = {
+            "Hey, how are you ?": "EN",
+            "Bonjour, comment ça va ?": "FR",
+            "Hola, ¿cómo estás?": "ES",
+            "こんにちは、お元気ですか?": "JA",
+        }
+
+        sentence_translated = [
+            translate_text(sentence, target_lang=target_lang)
+            for sentence in sentence_to_translate.keys()
+            for target_lang in languages
+            if sentence_to_translate[sentence] != target_lang
+        ]
+        is_all_sentence = all(
+            [isinstance(sentence, str) for sentence in sentence_translated]
+        )
 
         self.assertTrue(is_all_sentence)
 
-    def test_front_database_data(self)->None:
+    def test_front_database_data(self) -> None:
         """
         The goal of this function is to test
         whether the data loaded in the front
@@ -51,24 +68,47 @@ class Test(unittest.TestCase):
             -None
         """
 
-        result = subprocess.run(['docker', 'build', '-t', 'front_database_image:latest', '-f', 'Dockerfile-front-database', '.'], capture_output=True, text=True)
-        result_run = subprocess.run(['docker', 'run','-d', '-p', '3306:3306', '--name', 'local-mysql-container', 'front_database_image'], capture_output=True, text=True)
-        result_images = subprocess.run(['docker', 'images'], capture_output=True, text=True)
-        
-        
+        result = subprocess.run(
+            [
+                "docker",
+                "build",
+                "-t",
+                "front_database_image:latest",
+                "-f",
+                "Dockerfile-front-database",
+                ".",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        result_run = subprocess.run(
+            [
+                "docker",
+                "run",
+                "-d",
+                "-p",
+                "3306:3306",
+                "--name",
+                "local-mysql-container",
+                "front_database_image",
+            ],
+            capture_output=True,
+            text=True,
+        )
+
         load_data_to_front_database(kaggle_length=50000)
-        df_front_database=load_data()
-        
+        df_front_database = load_data()
+
         self.assertEqual(result.returncode, 0)
         self.assertEqual(result_run.returncode, 0)
         self.assertFalse(df_front_database.empty)
-        self.assertIn("french",df_front_database.columns)
-        self.assertIn("english",df_front_database.columns)
+        self.assertIn("french", df_front_database.columns)
+        self.assertIn("english", df_front_database.columns)
         self.assertEqual(2, df_front_database.shape[1])
 
-    def test_check_model_loading(self)->None:
+    def test_check_model_loading(self) -> None:
         """
-        The goal of this function is to 
+        The goal of this function is to
         check the ability to load a model
         from google cloud storage
 
@@ -78,15 +118,20 @@ class Test(unittest.TestCase):
             -None
         """
 
-        sys.path.insert(0, 'Deepl_reproduction')
-        bucket = client.get_bucket('english_deepl_bucket')
+        sys.path.insert(0, "Deepl_reproduction")
+        bucket = client.get_bucket("english_deepl_bucket")
         blobs = bucket.list_blobs()
         file_names = [blob.name for blob in blobs]
-        random_model=random.choice(file_names)
-        load_model()
-        #load_model(load_best=False,gcp_model_name=random_model)
+        random_model = random.choice(file_names)
+        best_model = load_model()
+        model_random = load_model(load_best=False, gcp_model_name=random_model)
 
-        self.assertTrue(os.path.exists(os.path.join('Deepl_reproduction/model',random_model)))
+        self.assertTrue(
+            os.path.exists(os.path.join("Deepl_reproduction/model", random_model))
+        )
+        self.assertIsInstance(model_random, dict)
+        self.assertIsInstance(best_model, dict)
+
 
 if __name__ == "__main__":
     unittest.main()
