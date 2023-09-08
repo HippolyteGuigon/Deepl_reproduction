14198d8adba8fccdbd13a2191e124adcfe23aaf1
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-09-08 18:38:39 +0200

    pushing_streamlit

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st 
 
-st.title("Deepl Reproduction project- Welcome page")
+st.title("Deepl Reproduction project")
+st.write("Welcome page")
+
+options = ["English", "Russian", "Chinese"]
+
+# Sélectionnez une option à partir de la liste déroulante
+selection = st.selectbox("Select the language you want to translate french to :", options)
+
+# Affichez la sélection
+st.write("You have chosen to translate from French to :", selection)
